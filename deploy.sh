1b1f8602bac6a9af18d0abfa0b9281e49dd595c1
BDS <dumitru.boila@gmail.com>
2019-03-29 20:12:35 +0200

    Addapted deploy script to take into account stopped containers too

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 docker build -t go-deploy-poc .
 readonly CONTAINER_NAME="gopoc"
 
-if [[ $(docker ps | grep ${CONTAINER_NAME} | wc -l) -eq 1 ]]; 
+if [[ $(docker ps -a | grep ${CONTAINER_NAME} | wc -l) -eq 1 ]]; 
 then
     echo "A container is already running. It will be stopped, removed and rexecuted."
     docker stop ${CONTAINER_NAME}
