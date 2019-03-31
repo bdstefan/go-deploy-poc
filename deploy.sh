254a7bfc7007173167fff47aa9aee10c1b33c822
BDS <dumitru.boila@gmail.com>
2019-03-31 21:57:41 +0300

    Updated deply script and removed unnecessary messages

--- deploy.sh
+++ deploy.sh
@@ -6,11 +6,11 @@ readonly EXPOSE_PORT=3030
 
 if [[ $(docker ps -a | grep ${CONTAINER_NAME} | wc -l) -eq 1 ]]; 
 then
-    echo "A container is already running. It will be stopped, removed and rexecuted."
-    docker stop ${CONTAINER_NAME}
-    docker rm ${CONTAINER_NAME}
+    echo "A ${CONTAINER_NAME} container already exists. It will be stopped if is running, will be removed and rexecuted."
+    docker stop ${CONTAINER_NAME} > /dev/null
+    docker rm ${CONTAINER_NAME} > /dev/null
 fi
 
-docker run --name ${CONTAINER_NAME} -d -p ${EXPOSE_PORT}:3030 go-deploy-poc
+docker run --name ${CONTAINER_NAME} -d -p ${EXPOSE_PORT}:3030 go-deploy-poc > /dev/null
 
 echo "The app is available at http://localhost:${EXPOSE_PORT}/liveness"
