603380402e4ffa0941407b30dca722b7dac8359b
BDS <dumitru.boila@gmail.com>
2019-03-31 21:13:29 +0300

    Renamed main handler

--- main.go
+++ main.go
@@ -7,13 +7,13 @@ import (
 	"strconv"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func indexHnalder(w http.ResponseWriter, r *http.Request) {
 	n, _ := strconv.Atoi(r.URL.Path[1:])
 	fmt.Fprintln(w, "Compute power for all ints up to", n)
 	compute(n, w)
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", indexHnalder)
 	log.Fatal(http.ListenAndServe(":3030", nil))
 }
